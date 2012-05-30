20dcc1fd5ff3a6c6925a7cd17605c259812b9f56
kenjikaneko <kenjikaneko0909@yahoo.co.jp>
2012-05-30 19:23:27 +0900

    aaa

--- TriangleTest.php
+++ TriangleTest.php
@@ -1,5 +1,45 @@
+<?php
 
+  require_once 'PHPUnit/Autoload.php';
+  require_once('Triangle.php');
 
+  class TriangleTest extends PHPUnit_Framework_TestCase {
 
+    public function setUp() {
+      $this->tri = new Triangle();
+    }
 
-
+    function testInt() {
+      try {
+        $this->tri->isInt(1, 1, 1);
+      } catch(Exception $e) {
+        $this->assertTrue(false,"整数ではありません");
+      }
+      $this->assertTrue(true);
+    }
+    function testOverZero() {
+      try {
+        $this->tri->isOverZero(3,3,3);
+      } catch(Exception $e) {
+        $this->assertTrue(false,"1以上ではありません");
+      }
+      $this->assertTrue(true);
+    }
+    function testCompareBarLength() {
+      try {
+        $this->tri->compareBarLength(3,3,3);
+      } catch(Exception $e) {
+        $this->assertTrue(false,"辺の和がおかしいです");
+      }
+      $this->assertTrue(true);
+    }
+    function testCheckShapeTriangle() {
+      try{
+        $this->tri->checkShapeTriangle(1,3,2);
+      } catch(Exception $e) {
+        echo $e->getMessage();
+        $this->assertTrue(true);
+      }
+    }
+  }
+?>
--- triangle.php
+++ triangle.php
@@ -1,6 +1,44 @@
+<?php
+class Triangle {
+  function isInt($a,$b,$c) {
+    $a_int = is_int($a);
+    $b_int = is_int($b);
+    $c_int = is_int($c);
 
-
-
-
-
-
+    if(!$a_int || !$b_int || !$c_int) {
+       throw new Exception('error');
+    }
+  }
+  function isOverZero($a,$b,$c) {
+    if($a<=0) {
+       throw new Exception('error');
+    }
+    if($b<=0) {
+       throw new Exception('error');
+    }
+    if($c<=0){
+       throw new Exception('error');
+    }
+  }
+  function compareBarLength($a,$b,$c) {
+    if($a>=($b+$c)) {
+      throw new exception('error');
+    }
+    if($b>=($c+$a)) {
+      throw new exception('error');
+    }
+    if($c>=($a+$b)) {
+      throw new exception('error');
+    }
+  }
+  function checkShapeTriangle($a,$b,$c) {
+    if(($a==$b) && ($b==$c) && ($c==$a)) {
+      throw new exception('正三角形です');
+    } elseif(($a==$b) || ($b==$c) || ($c==$a)) {
+      throw new exception('二等辺三角形です');
+    } else {
+      throw new exception('普通の三角形です');
+    }
+  }
+}
+?>
